6f2a0ad7d155b4aa9758789775284b9c6d8c77d5
Félix Domingos <felixsdomingos93@gmail.com>
2024-12-19 16:07:15 +0100

    modify the code to use the build script and create a new build to deploy

--- dist/src/app.js
+++ dist/src/app.js
@@ -9,10 +9,22 @@ const emailRoutes_1 = __importDefault(require("./routes/emailRoutes"));
 const app = (0, express_1.default)();
 const cors_1 = __importDefault(require("cors"));
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type']
 }));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     const allowedOrigins = ['http://localhost:5173', 'https://bit-go.vercel.app'];
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Origin not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type']
+// }));
 app.use(express_1.default.json());
 app.use('/viagens', viagemRoutes_1.default);
 app.use('/email', emailRoutes_1.default);
